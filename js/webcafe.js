1ed831146b8493cac278b427fa96c74c89c5af65
flame3750 <flame3750@gmail.com>
2019-10-30 09:50:25 +0900

    헤더 디자인 완성 및 비주얼 마크업 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,7 +1,19 @@
 var member_lists = $('.member li:nth-child(n+2)');
+var menu_items = $('.menu-item');
 var btn_menu = $('.btn-menu');
 var submenu_lists = $('.sub-menu li');
 
 member_lists.prepend('<span aria-hidden="true">:</span>');
 btn_menu.attr('role', 'button');
 submenu_lists.addClass('icon-dot-circled');
+
+btn_menu.on('click', function(e){
+  e.preventDefault();
+  if($(this).parent().hasClass('menu-act')){
+    $(this).parent().removeClass('menu-act');
+  }
+  else{
+    menu_items.removeClass('menu-act');
+    $(this).parent().addClass('menu-act');
+  }
+});
