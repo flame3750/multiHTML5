205ce4492432ad73d6603f9f0e78e21b72b10fdb
flame3750 <flame3750@gmail.com>
2019-10-31 16:00:03 +0900

    공지사항 및 자료실 인터렉션 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -2,6 +2,8 @@ var member_lists = $('.member li:nth-child(n+2)');
 var menu_items = $('.menu-item');
 var btn_menu = $('.btn-menu');
 var submenu_lists = $('.sub-menu li');
+var sections = $('.notice, .pds');
+var tabs = $('.tab');
 
 member_lists.prepend('<span aria-hidden="true">:</span>');
 btn_menu.attr('role', 'button');
@@ -16,4 +18,10 @@ btn_menu.on('click', function(e){
     menu_items.removeClass('menu-act');
     $(this).parent().addClass('menu-act');
   }
+});
+
+tabs.on('click', function(e){
+  e.preventDefault();
+  sections.removeClass('tab-act');
+  $(this).parent().parent().addClass('tab-act');
 });
